871f4e63db1bd1deccf17689d96fdffa4c7cda59
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-02-24 00:22:43 +0530

    Use for sample loopylighthouse

---
+++ loopylighthouse.js
@@ -0,0 +1,21 @@
+let x = 100;
+while(x<=200)
+{
+ if(x % 3 === 0 && x % 4 === 0)
+ {
+    console.log("LoopyLighthouse");
+ }   
+ else if(x % 4 === 0)
+ {
+    console.log("Lighthouse");
+ }
+ else if(x % 3 === 0)
+ {
+    console.log("Loopy");
+ }
+ else
+ {
+ console.log(x);
+ }
+ x = x + 1;
+}
